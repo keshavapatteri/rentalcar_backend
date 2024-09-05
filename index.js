a5e5263ac7940631dd8e75820a04fb755467417d
keshavapatteri <mktkid2023@gmail.com>
2024-09-05 15:11:28 +0530

    cc

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://rentalcarsauto.netlify.app',
+    origin: 'https://rentalcar-frontend-keshavapatteris-projects.vercel.app',
     credentials: true,
 }));
 
